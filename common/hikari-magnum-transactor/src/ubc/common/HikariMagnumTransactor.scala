983f7a31f088f39b9820fde9931102bdd5b077ee
Matt Langsenkamp <ml2513@rit.edu>
2026-04-16 22:28:20 -0400

    hc

--- common/hikari-magnum-transactor/src/ubc/common/HikariMagnumTransactor.scala
+++ common/hikari-magnum-transactor/src/ubc/common/HikariMagnumTransactor.scala
@@ -1,6 +1,7 @@
 package ubc.common
 
 import com.augustnagro.magnum.magzio.TransactorZIO
+import com.augustnagro.magnum.sql
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import zio.*
 import zio.config.magnolia.*
@@ -8,10 +9,10 @@ import zio.config.magnolia.*
 import javax.sql.DataSource
 
 private final case class DatabaseConfig(
-  postgresHost: String,
-  postgresDb: String,
-  postgresUser: String,
-  postgresPassword: String
+    postgresHost: String,
+    postgresDb: String,
+    postgresUser: String,
+    postgresPassword: String
 ) derives Config
 
 object HikariMagnumTransactor:
@@ -24,16 +25,20 @@ object HikariMagnumTransactor:
       for
         cfg  <- ZIO.service[DatabaseConfig]
         pool <- ZIO.acquireRelease(
-                  ZIO.attempt {
-                    val hc = new HikariConfig()
-                    hc.setJdbcUrl(s"jdbc:postgresql://${cfg.postgresHost}:5432/${cfg.postgresDb}")
-                    hc.setUsername(cfg.postgresUser)
-                    hc.setPassword(cfg.postgresPassword)
-                    new HikariDataSource(hc)
-                  }
-                )(p => ZIO.attempt(p.close()).orDie)
+          ZIO.attempt {
+            val hc = new HikariConfig()
+            hc.setJdbcUrl(s"jdbc:postgresql://${cfg.postgresHost}:5432/${cfg.postgresDb}")
+            hc.setUsername(cfg.postgresUser)
+            hc.setPassword(cfg.postgresPassword)
+            new HikariDataSource(hc)
+          }
+        )(p => ZIO.attempt(p.close()).orDie)
       yield pool
     )
 
   val layer: TaskLayer[TransactorZIO] =
-    configLayer >>> dataSourceLayer >>> TransactorZIO.layer
+    (configLayer >>> dataSourceLayer >>> TransactorZIO.layer).tap(z =>
+      z.get[TransactorZIO].connect {
+        sql"SELECT 1".query[Int].run()
+      }
+    )
